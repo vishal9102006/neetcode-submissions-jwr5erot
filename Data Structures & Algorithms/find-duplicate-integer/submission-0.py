eac6d2c8a2ed71d4e3892d000405db686cbff59b
vishal9102006 <164604495+vishal9102006@users.noreply.github.com>
2026-08-24 20:47:48 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[0]
+
+        # Find the intersection point
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        # Find the duplicate number
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
